5a320062d85df932623e04acb9b4db582a038c30
Mayur Vijay Kode <mayurkode7@gmail.com>
2025-08-13 19:15:15 +0530

    Remove unused SVG files and update layout and page components to reflect the new app title and description for Calculator Plus.

---
+++ calculator-plus-app/src/app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">About Calculator Plus</h1>
+      <p className="text-lg">
+        This is a simple calculator app built with Next.js and Tailwind CSS.   
+      </p>
+    </div>
+  );
+}
---
+++ calculator-plus-app/src/app/percentage/page.jsx
@@ -0,0 +1,10 @@
+export default function Percentage() {
+  return(
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Percentage</h1>
+      <p className="text-lg">
+        This page offers simple percentage tools to help you quickly calculate discounts, increases, and proportions.
+      </p>
+    </div>
+  );
+}
